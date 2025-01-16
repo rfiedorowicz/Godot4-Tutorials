7a93d0dd3cc788f572d72946962afbedfbb8917e
Rafal F <fiedorowicz.rafal@gmail.com>
2025-01-16 19:11:48 +0100

    add basic movement animation

--- game/scenes/characters/player/player.gd
+++ game/scenes/characters/player/player.gd
@@ -1,23 +1,44 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED := 400.0
+const JUMP_VELOCITY := -1200.0
+const ROTATION_SPEED := 0.3
+
+var is_look_left := false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	calculate_velocity(delta)
+	animate()
+	
+	move_and_slide()
+
+func calculate_velocity(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("game_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("game_left", "game_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+func animate():
+	face_good_direction()
+	if velocity.x == 0:
+		$AnimationPlayer.play("Idle")
+		return
+	
+	is_look_left = velocity.x < 0
+
+	$AnimationPlayer.play("Walk")
+
+func face_good_direction() -> void:
+	if is_look_left:
+		var new_value = ($Sprites.scale.x - ROTATION_SPEED)
+		$Sprites.scale.x = [new_value, -1].max()
+	else:
+		var new_value = ($Sprites.scale.x + ROTATION_SPEED)
+		$Sprites.scale.x = [new_value, 1].min()
